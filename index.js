397e3c2bc65d0e7f8df8a8c539759c3a30e6f49a
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-13 06:21:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  let newObject = object
+  let newObject = Object.assign({}, object)
   console.log('Before')
   console.log(object)
   console.log(newObject)
